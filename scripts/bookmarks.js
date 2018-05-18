6b7c695e98d1e143afde79ea997e1104509a24cd
James Stuckey <jstuckey@rutgers.edu>
2018-05-18 11:31:45 -0400

    updated addBookmark html template

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -12,24 +12,31 @@ const bookmarks = (function (){
         });
     }
 
-    function generateAddingNewBookmarkHtml(){
-        return `<input type="text" name="article-title" class="article-title" placeholder="title of article">
-        <input type="text" name="article-url" class="article-url" placeholder="http://www.example.com">`;
-    }
+    // function generateAddingNewBookmarkHtml(){
+    //     return ` <div class="input-form" id="input-form">
+    //     <div class="article-title-container">
+    //         <label for="article-title" class="title-label">Title:</label>
+    //         <input type="text" name="article-title" class="article-title" placeholder="title of article">
+    //     </div>
+
+    //     <div class="article-url-container">
+    //         <label for="article-url" class="url-label">Url:</label>
+    //         <input type="text" name="article-url" class="article-url" placeholder="http://www.example.com">
+    //     </div>
+
+    //     <div>
+    //         <label for="desc-box" class="desc-box">Description:</label>
+    //         <input type="text" name="desc-box" class="desc-box" placeholder="Insert description text here">
+    //     </div>
+    // </div>`;
+    // }
 
     function render() {
         let bookmarks = store.items;
 
         // if (store.addingBookmark) {
-        //     generateAddingNewBookmarkHtml();
+        //     $('.js-bookmarks').html(generateAddingNewBookmarkHtml());
         // }
-        // use jquery html() to append generated form on the html page after add button is clicked
-
-        if (store.addingBookmark) {
-            $('.js-bookmarks').html(generateAddingNewBookmarkHtml());
-        } else {
-            $('.js-bookmarks').html();
-        }
     }
 
     function bindEventListeners(){
